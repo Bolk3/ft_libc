bfb22333466c06206de5b2a072599469cf0b39de
Bolk3 <renald.sonnet59@gmail.com>
2025-10-17 05:06:26 +0200

    updated the libft.h

--- libft.h
+++ libft.h
@@ -1,3 +1,4 @@
+#include <cstddef>
 #define LIBFT_H
 #ifdef LIBFT_H
 
@@ -27,5 +28,28 @@ int ft_ispunct(int c);
 int ft_isspace(int c);
 int ft_isupper(int c);
 int ft_isxdigit(int c);
+
+int ft_atoi(char const *source);
+char *ft_itoa(int n);
+
+int ft_toupper(int c);
+int ft_tolower(int c);
+int ft_strlen(char const *source);
+char *ft_strdup(char const *source);
+char *ft_strcpy(char *destination, char const *source);
+char *ft_strncpy(char *destination, char const *source, size_t lenght);
+char *ft_strcat(char *destination, char const *source);
+char *ft_strncat(char *destination, char const *source, size_t lenght);
+size_t ft_strlcat(char *destination, char const *source, size_t lenght);
+char *ft_strchr(char const *string, int searchedChar);
+char *ft_strrchr(char const *string, int searchedChar);
+char *ft_strstr(char *fullString, char *substring);
+char *ft_strnstr(char *fullString, char *substring);
+int ft_strcmp(char const *first, char const *second);
+int ft_strncmp(char const *first, char const *second, size_t lenght);
+
+void *ft_memset(void *pointer, int value, size_t count);
+void ft_bzero(void *s, size_t n);
+void *ft_memcpy(void *destination, void const *source, size_t size);
 
 #endif // LIBFT_H
